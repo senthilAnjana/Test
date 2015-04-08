ee869888b1faebbd045b608be7423274b8ea08b1
senthil <senthilc@vishwak.com>
2015-04-08 14:50:16 +0530

    test commit

--- HelloEgit/src/com/example/helloegit/MainActivity.java
+++ HelloEgit/src/com/example/helloegit/MainActivity.java
@@ -2,14 +2,14 @@ package com.example.helloegit;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	Button b;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+	
 		setContentView(R.layout.main);
 		
 	}
