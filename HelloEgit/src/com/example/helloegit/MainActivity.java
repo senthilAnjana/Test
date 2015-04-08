c5d6f81f968fad403bf4ca77ea1a73c0347314c8
senthil <senthilc@vishwak.com>
2015-04-08 15:01:26 +0530

    Toast

--- HelloEgit/src/com/example/helloegit/MainActivity.java
+++ HelloEgit/src/com/example/helloegit/MainActivity.java
@@ -3,14 +3,16 @@ package com.example.helloegit;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	Button b1;
+	TextView t;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	
 		setContentView(R.layout.main);
-		
+		Toast.makeText(this, "Hi", Toast.LENGTH_LONG).show();
 	}
 }
